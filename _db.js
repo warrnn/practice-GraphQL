28ec2737e968449658b34b8c85696038b21910b8
Bryan Mogens Warren <bryanwarren86@gmail.com>
2025-01-20 20:56:46 +0700

    refactor: update book and member IDs to use a consistent format and add queries for individual book and member retrieval

--- _db.js
+++ _db.js
@@ -1,6 +1,6 @@
 const books = [
 	{
-		id: 1,
+		id: "book_1",
 		title: "Harry Potter and the Chamber of Secrets",
 		author: "J.K. Rowling",
 		published_at: "1998-07-02",
@@ -8,7 +8,7 @@ const books = [
 		total: 10,
 	},
 	{
-		id: 2,
+		id: "book_2",
 		title: "Harry Potter and the Philosopher's Stone",
 		author: "J.K. Rowling",
 		published_at: "1997-06-26",
@@ -19,9 +19,15 @@ const books = [
 
 const members = [
 	{
-		id: "1",
+		id: "member_1",
 		name: "Bryan Mogens Warren",
-		email: "bryanwarren86@gmail.com",
+		email: "bryanwarren@gmail.com",
+		verified: true,
+	},
+	{
+		id: "member_2",
+		name: "Dadang Sumedang",
+		email: "dadangsumendang@gmail.com",
 		verified: true,
 	},
 ];
